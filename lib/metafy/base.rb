86d41d8dad6e4ada128261906f95529c85519b24
Blair Williams <supercleanse@gmail.com>
2011-05-07 16:14:24 -0600

    Fixed an issue with metafy::Base added documentation

--- lib/metafy/base.rb
+++ lib/metafy/base.rb
@@ -107,50 +107,7 @@ class << ActiveRecord::Base
     @metafied || false
   end
 
-  def meta_attrs( *options )
-    cattr_accessor :metafied_attrs
-    self.metafied_attrs = options
-
-    # Sets to determine what classes have been metafied
-    @metafied = self.metafied_attrs.count > 0
-
-    meta_joins = []
-    class_name = self.name.classify
-    table_name = self.table_name
-    meta_selects = ["#{table_name}.*"]
-    self.metafied_attrs.each do |col|
-      meta_joins.push("LEFT JOIN metas m_#{col.to_s} ON m_#{col.to_s}.target_id=#{table_name}.id AND m_#{col.to_s}.meta_key='#{col.to_s}' AND m_#{col.to_s}.target_type='#{class_name}'")
-      meta_selects.push("m_#{col.to_s}.meta_value AS #{col.to_s}")
-    end
-
-    default_scope :select => meta_selects, :joins => meta_joins
-
-    scope :meta_where, lambda { |options|
-      conditions = {}
-      options.each_pair do |key,val|
-        conditions["m_#{key.to_s}".to_sym] = { :meta_value => val }
-      end
-
-      where(conditions)
-    }
-    
-    has_many :metas, :as => :target
-
-    options.each do | meta_attr |
-      attr_accessor meta_attr.to_sym
-    end
-
-    include Metafy::Base
-  end
-end
-
-# Adds the metafy_atts method in ActiveRecord::Base, which can be used to define the metafied objects.
-class << ActiveRecord::Base
-  def metafied?
-    @metafied || false
-  end
-
-  def meta_attrs( *options )
+  def metafy( *options )
     cattr_accessor :metafied_attrs
     self.metafied_attrs = options
 
--- metafy.gemspec
+++ metafy.gemspec
@@ -6,18 +6,19 @@ Gem::Specification.new do |s|
   s.name        = "metafy"
   s.version     = Metafy::VERSION
   s.platform    = Gem::Platform::RUBY
-  s.authors     = ["Caseproof, LLC"]
+  s.authors     = ["supercleanse"]
   s.email       = ["support@caseproof.com"]
-  s.homepage    = "http://caseproof.com"
+  s.homepage    = "http://blairwilliams.com/rails/metafy"
   s.summary     = %q{Meta Attributes Engine for Rails 3}
-  s.description = %q{Metafy makes it possible to easily add dynamic attributes to a model that are fully searchable.}
+  s.description = %q{Metafy makes it possible to easily add dynamic attributes to any ActiveRecord model. These dynamic attributes work just like normal database column attributes and are fully searchable (i.e. You can find records by values contained in these dynamic attributes).}
 
-  s.add_dependency 'rails'
+  s.add_dependency('rails','>= 3.0.3')
+  s.license = 'MIT'
 
   s.rubyforge_project = "metafy"
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
-  s.require_paths = ["app","config","lib"]
+  #s.require_paths = ["app","config","lib"]
 end
