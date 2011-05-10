d94a54ac73bf34da24304f426b1e9157c01aa955
Blair Williams <supercleanse@gmail.com>
2011-05-10 12:58:06 -0600

    Refactored some of the code

--- lib/metafy.rb
+++ lib/metafy.rb
@@ -1,4 +1,4 @@
 module Metafy
   require 'metafy/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
-  require 'metafy/base' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  require 'metafy/activerecord_methods' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
